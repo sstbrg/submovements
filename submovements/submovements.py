15009df69248ffdcbc3016fc48636a7bcb633fe2
Stas <stanislavs1@mail.tau.ac.il>
2019-06-19 11:53:38 +0300

    Added lowpass 5hz, windowing around motion data
    Lowpass 5hz on position data -> Savitsky-Golay derivative -> smooth velocity.
    
    Data is found using threshold on |V|^2 with +/- 0.2ms window around it.

--- submovements/submovements.py
+++ submovements/submovements.py
@@ -4,6 +4,7 @@ from DataProcessing import Preprocessor, Trial
 
 path_to_raw_data = '..\\data\\results\\simonvisual\\1'
 path_to_database = '..\\data\\processed_data.mat'
+path_to_trials = '..\\data\\trials'
 #sample_rate = 240 #S/s
 
 pproc = Preprocessor()
@@ -12,8 +13,9 @@ raw_data_gen = pproc.load_df_from_directory_gen(dir_path=path_to_raw_data)
 
 for trial in raw_data_gen:
     trial.preprocess(pproc, axes=['x','y'])
-    pproc.plot(trial.velocity_data[['x', 'y']], mode='pandas')
-    pproc.plot(trial.filtered_velocity_data[['x', 'y']], mode='pandas')
+    trial.save_as_csv(path_to_trials)
+    pproc.plot(trial.velocity_data[['x', 'y']])
+    pproc.plot(trial.filtered_velocity_data[['x', 'y']])
 
-    print(1)
+    print(trial)
     break
