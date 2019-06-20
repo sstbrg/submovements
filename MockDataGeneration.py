18e84916efd407ad5863023e8166629ed3151fd8
lizi12 <liz.izakson@mail.huji.ac.il>
2019-06-20 09:27:40 +0300

    erase mock data

--- MockDataGeneration.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-df = pd.DataFrame(np.random.rand(100, 5)).rename(columns={0: 'a', 1: 'b', 2: 'c'}).set_index(['a', 'b', 'c'])
-
-df
